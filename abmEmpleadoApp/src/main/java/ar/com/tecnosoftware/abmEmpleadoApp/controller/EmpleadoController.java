576f46f11383787347fec507823dc44d834785e7
MariaJuarez <juarezmaria2601@gmail.com>
2018-07-23 10:05:49 -0300

    Se cambia los valores en el RequestMapping y se retira el Qualifier ya que solo se usa una vez

--- abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/abmEmpleadoApp/controller/EmpleadoController.java
+++ abmEmpleadoApp/src/main/java/ar/com/tecnosoftware/abmEmpleadoApp/controller/EmpleadoController.java
@@ -23,12 +23,11 @@ import java.util.Map;
 public class EmpleadoController {
 
     @Autowired
-    @Qualifier("empleadoService")
     private EmpleadoService empleadoService;
 
     //Metodo para crear empleado
     //RequestMapping le digo el valor del mapping y en este caso  su metodo va a agregar un dato, es por ello el POST
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/crear", method = RequestMethod.POST)
     /*
      *Con el @Valid verifico si el dato que entra al parámetro sea correcto
      * y el @RequestBody indico que el parámetro de método debe estar vinculado al cuerpo de la solicitud web
@@ -39,19 +38,21 @@ public class EmpleadoController {
 
     /*        Metodo para editar empleado
     * En el @RequestMapping le enviamos un RequestMethod.PUT para editar
-    * Con @PathVariable le indico el el valor que va a entrar viene en la URL
     * Con @RequestBody indico que el parámetro de método debe estar vinculado al cuerpo de la solicitud web
      Con ResponseEntity<> le valido y entrego el estatus http del objeto enviado*/
-    @RequestMapping(value = "/edit/{empleadoId}", method = RequestMethod.PUT)
-    public ResponseEntity<Empleado> editEmpleado(@PathVariable ("empleadoId") int empleadoId ,@Valid @RequestBody Empleado empleadoEdited) throws EmpleadoNotFoundException {
-        Empleado empleado = empleadoService.searchEmpleado(empleadoId);
-        if (empleadoService.editEmpleado(empleadoId, empleadoEdited)) {
+    @RequestMapping(value = "/editar", method = RequestMethod.PUT)
+    public ResponseEntity<Empleado> editEmpleado(@Valid @RequestBody Empleado empleadoEdited) throws EmpleadoNotFoundException {
+        if (empleadoService.editEmpleado(empleadoEdited.getId(), empleadoEdited)) {
             return ResponseEntity.ok(empleadoEdited);
         }
-        throw new EmpleadoNotFoundException("No se encontro el empleado con el id: " + empleadoId);
+        throw new EmpleadoNotFoundException("No se encontro el empleado con el id: " + empleadoEdited.getId());
     }
-    //Metodo para eliminar empleado con el parametro ID
-    @RequestMapping(value = "/delete/{empleadoId}", method = RequestMethod.DELETE)
+
+
+/*    Metodo para eliminar empleado con el parametro ID
+        * Con @PathVariable le indico que el valor que va a entrar viene en la URL */
+
+    @RequestMapping(value = "/eliminar/{empleadoId}", method = RequestMethod.DELETE)
     public ResponseEntity<Empleado> deleteEmpleado(@PathVariable ("empleadoId") int empleadoId) throws EmpleadoNotFoundException {
         Empleado empleado=empleadoService.searchEmpleado(empleadoId);
         if(empleadoService.deleteEmpleado(empleadoId)){
